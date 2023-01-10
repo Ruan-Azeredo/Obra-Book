2d056e5d42805faf22221baf2c9cd90a7f7ca2c2
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-10 14:13:38 -0300

    :constrcution: build: adiciona rotas em app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Sidebar from './components/Sidebar';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      Hello world
+    <div className='flex'>
+      <BrowserRouter>
+        <div>
+          <Sidebar/>
+        </div>
+        <div>
+          <Routes>
+            <Route element={<Dashboard/>} path='/dashboard'/>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App;
