f918f8ea6a2290ac3fc73b926ac3200a8b1b0102
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-10 15:03:41 -0300

    :construction: build: sidebar logic

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { chartIcon, groupIcon, wrenchIcon } from './icons'
+
+const items = [
+  { link: '/dashboard', name: 'Dashboard', icon: chartIcon },
+  { link: '/funcionarios', name: 'Funcionários', icon: groupIcon },
+  { link: '/ferramentas', name: 'Ferramentas', icon: wrenchIcon }
+]
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='flex flex-col'>
+      {items.map(item => (
+        <NavLink to={item.link}
+        className={({isActive}) => isActive ? 'bg-slate-500' : 'bg-red-500'}
+        >
+          <div>{item.icon}</div>
+          <div>{item.name}</div>
+        </NavLink>
+      ))}
+    </div>
   )
 }
 
