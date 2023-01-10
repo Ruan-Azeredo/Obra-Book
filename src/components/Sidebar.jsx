407c71342fc3e4906744adc8ccb38694bcdf56b2
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-10 15:25:38 -0300

    :lipstick: style: estiliza sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -10,15 +10,33 @@ const items = [
 
 const Sidebar = () => {
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col sidebar'>
       {items.map(item => (
         <NavLink to={item.link}
-        className={({isActive}) => isActive ? 'bg-slate-500' : 'bg-red-500'}
+        className={({isActive}) => isActive ? 'bg-white item' : 'text-white item'}
         >
           <div>{item.icon}</div>
           <div>{item.name}</div>
         </NavLink>
       ))}
+    <style jsx>{`
+        .sidebar{
+          margin: 15px;
+          background: var(--mainColor);
+          padding: 15px;
+          display: flex;
+          height: calc(100vh - 30px);
+          border-radius: 30px;
+          grid-gap: 15px
+        }
+        .item{
+          display: flex;
+          padding: 10px 15px;
+          width: 200px;
+          grid-gap: 20px;
+          border-radius: 15px
+        }
+    `}</style>
     </div>
   )
 }
