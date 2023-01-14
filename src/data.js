b2252d4230a68e284e9df1426f684310ebfd80df
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-13 22:51:50 -0300

    :construction: build: add data array

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const funcionarios = [
+    {
+        id: 1,
+        name: 'Ruan',
+        lastName: 'Azeredo',
+        phone: '(27) 9962-7180',
+        type: 'Pedreiro',
+        salary: '120',
+        status: 'Ativo',
+    },
+    {
+        id: 2,
+        name: 'Lazaro',
+        lastName: 'Ramos',
+        phone: '(27) 9962-7890',
+        type: 'Ajudante',
+        salary: '90',
+        status: 'Desativo',
+    },
+    {
+        id: 3,
+        name: 'Manoel',
+        lastName: 'Gomes',
+        phone: '(27) 9964-7901',
+        type: 'Pintor',
+        salary: '130',
+        status: 'Ativo',
+    }
+]
