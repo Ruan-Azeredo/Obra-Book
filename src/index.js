4873d21663334acb0c6a878bcb378f84c7f12355
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-13 22:52:28 -0300

    :sparkles: feat: adiciona theme dark index.js

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { NextUIProvider } from '@nextui-org/react';
+import { nextTheme } from './nextui';
+
+
+const darkTheme = nextTheme
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NextUIProvider>
+    <NextUIProvider theme={darkTheme}>
       <App />
     </NextUIProvider>
   </React.StrictMode>
