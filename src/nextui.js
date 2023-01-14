f56aa954f9375f19ddd55ed673f29d181d4b8a00
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-13 22:52:45 -0300

    :construction: build: nextui.js

---
+++ src/nextui.js
@@ -0,0 +1,10 @@
+import { createTheme } from "@nextui-org/react";
+
+export const nextTheme = createTheme({
+  type: 'dark',
+  theme: {
+    colors: {
+      primary: 'var(--mainColor)',
+    }
+  }
+})
