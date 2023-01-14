9ba2e021046bb092ab61f49e531b173867ffd830
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-13 22:49:54 -0300

    :sparkles: feat: theme colors tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#080911',
+        mainColor: '#3d54ba',
+        darkGray: '#3d3d4e',
+        lightGray: '#4f4756',
+        green: '#4EBA3D',
+        red: '#E54F6D'
+      },
+    },
   },
   plugins: [],
 }
