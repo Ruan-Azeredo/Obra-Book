be79e5f0d6a93ec133820b67bfea3d95c8d45974
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-18 18:12:39 -0300

    :sparkles: feat: logica qtActive/qtTotal

--- src/components/functions/HeaderCrud.jsx
+++ src/components/functions/HeaderCrud.jsx
@@ -1,13 +1,21 @@
-import { Row } from "@nextui-org/react";
 import ModalButton from "../ModalButton";
 
-export default function HeaderFuncionarios() {
+export default function HeaderFuncionarios(props) {
+    const data = props.data
+    var qtTotal = 0
+    var qtActive = 0
+
+    data.map(item => {
+        qtTotal++
+        if(item.status == 'Ativo'){qtActive++}
+    })
+
     return (
         <div className='bg-mainColor w-[95%] flex mx-auto z-20 relative rounded-[15px] justify-between'>
             <div className="bg-white m-4 p-3 gap-4 flex text-center rounded-lg text-background">
                 <div className="text-start font-semibold flex h-fit self-center">Funcionários Ativos:</div>
                 <div>
-                    <span className="text-3xl text-mainColor font-bold">24</span><span className="text-gray-400 font-medium">/36</span>
+                    <span className="text-3xl text-mainColor font-bold">{qtActive}</span><span className="text-gray-400 font-medium">/{qtTotal}</span>
                 </div>
             </div>
             <div className='m-4'>
