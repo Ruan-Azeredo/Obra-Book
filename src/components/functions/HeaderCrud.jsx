0aa719c2416563e3fa2cccb72051868105e9a815
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-18 18:02:59 -0300

    :recycle: refactor: reduz header crud

--- src/components/functions/HeaderCrud.jsx
+++ src/components/functions/HeaderCrud.jsx
@@ -3,14 +3,16 @@ import ModalButton from "../ModalButton";
 
 export default function HeaderFuncionarios() {
     return (
-        <div className='bg-mainColor w-[95%] flex mx-auto h-40 z-20 relative rounded-[15px]'>
-            <div className="bg-white w-32 m-4 flex flex-col text-center rounded-lg text-background">
-                <div className="text-start m-3 font-semibold">Funcionários Ativos:</div>
+        <div className='bg-mainColor w-[95%] flex mx-auto z-20 relative rounded-[15px] justify-between'>
+            <div className="bg-white m-4 p-3 gap-4 flex text-center rounded-lg text-background">
+                <div className="text-start font-semibold flex h-fit self-center">Funcionários Ativos:</div>
                 <div>
                     <span className="text-3xl text-mainColor font-bold">24</span><span className="text-gray-400 font-medium">/36</span>
                 </div>
             </div>
-            <ModalButton type='Funcionário' action='adiciona' data={undefined} />
+            <div className='m-4'>
+                <ModalButton type='Funcionário' action='adiciona' data={undefined}/>
+            </div>
         </div>
     )
 }
