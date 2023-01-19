184a7370319a6bf9f97262fa3879b2a157a0fa17
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-19 09:42:42 -0300

    :sparkles: chore: add mais infos a funcionarios

--- src/data.js
+++ src/data.js
@@ -1,12 +1,19 @@
 export const funcionarios = [
     {
         id: 1,
+        engenheiro_id: 1,
         name: 'Ruan',
         lastName: 'Azeredo',
+        salary: 120,
         phone: '(27) 9962-7180',
         type: 'Pedreiro',
-        salary: '120',
         status: true,
+        cpf: '167.200.697-05',
+        age: 34,
+        street: 'Louzival Carvalho',
+        district: 'Guriri',
+        number: 60,
+        zip_code: 29934738
     },
     {
         id: 2,
