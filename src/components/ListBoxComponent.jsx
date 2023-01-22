20316d35547b9dcf751da2da66932468a833a535
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-22 17:07:25 -0300

    :construction: build: adiciona lock a input select

--- src/components/ListBoxComponent.jsx
+++ src/components/ListBoxComponent.jsx
@@ -4,11 +4,12 @@ import { checkIcon, chevronUpDownIcon } from './icons'
 
 export default function ListBoxComponent(props) {
   const items = props.items
+  const lock = props.lock
   const [selected, setSelected] = useState(items[0])
 
   return (
     <div className="w-64">
-      <Listbox value={selected} onChange={setSelected}>
+      <Listbox value={selected} onChange={setSelected} disabled={lock}>
         <div className="relative mt-1">
           <Listbox.Button className="relative w-full cursor-default rounded-lg bg-lightGray py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-mainColor sm:text-sm">
             <span className="block truncate text-white">{selected.name}</span>
