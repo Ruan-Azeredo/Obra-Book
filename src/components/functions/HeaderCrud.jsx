fe7316c3cf0afa4bcde241421d3fd5e53738815c
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-22 17:08:36 -0300

    :construction: build: constroi header ferra e mate

--- src/components/functions/HeaderCrud.jsx
+++ src/components/functions/HeaderCrud.jsx
@@ -1,6 +1,6 @@
 import ModalButton from "../ModalButton";
 
-export default function HeaderFuncionarios(props) {
+export function HeaderFuncionarios(props) {
     const data = props.data
     var qtTotal = 0
     var qtActive = 0
@@ -20,6 +20,52 @@ export default function HeaderFuncionarios(props) {
             </div>
             <div className='m-4'>
                 <ModalButton type='Funcionário' action='adiciona' data={undefined}/>
+            </div>
+        </div>
+    )
+}
+
+export function HeaderFerramentas(props) {
+    const data = props.data
+    var qtTotal = 0
+
+    data.map(item => {
+        qtTotal++
+    })
+
+    return (
+        <div className='bg-mainColor w-[95%] flex mx-auto z-20 relative rounded-[15px] justify-between'>
+            <div className="bg-white m-4 p-3 gap-4 flex text-center rounded-lg text-background">
+                <div className="text-start font-semibold flex h-fit self-center">Quantidade de Ferramentas:</div>
+                <div>
+                    <span className="text-3xl text-mainColor font-bold">{qtTotal}</span>
+                </div>
+            </div>
+            <div className='m-4'>
+                <ModalButton type='Ferramenta' action='adiciona' data={undefined}/>
+            </div>
+        </div>
+    )
+}
+
+export function HeaderMateriais(props) {
+    const data = props.data
+    var qtTotal = 0
+
+    data.map(item => {
+        qtTotal++
+    })
+
+    return (
+        <div className='bg-mainColor w-[95%] flex mx-auto z-20 relative rounded-[15px] justify-between'>
+            <div className="bg-white m-4 p-3 gap-4 flex text-center rounded-lg text-background">
+                <div className="text-start font-semibold flex h-fit self-center">Quantidade de Materiais:</div>
+                <div>
+                    <span className="text-3xl text-mainColor font-bold">{qtTotal}</span>
+                </div>
+            </div>
+            <div className='m-4'>
+                <ModalButton type='Material' action='adiciona' data={undefined}/>
             </div>
         </div>
     )
