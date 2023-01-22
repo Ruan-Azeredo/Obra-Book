ce36660f0663390a4e5a0529c154006834fa9ae1
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-22 17:09:02 -0300

    :construction: build: constroi inputs ferra e mate

--- src/components/functions/InputsModais.jsx
+++ src/components/functions/InputsModais.jsx
@@ -1,6 +1,8 @@
 import { Input, Modal, Switch } from "@nextui-org/react"
+import { funcionarios, obras } from "../../data"
+import ListBoxComponent from "../ListBoxComponent"
 
-export default function FuncionariosModalBody(props) {
+export function FuncionariosModalBody(props) {
     const data = props.data
     const footer = props.footer
     const action = props.action
@@ -92,10 +94,10 @@ export default function FuncionariosModalBody(props) {
                     size="lg"
                     labelPlaceholder="Tipo"
                     />
-                    <Switch className="mx-20"/>
+                    <ListBoxComponent lock={lock} items={[{name: 'Ativo', value: true}, {name: 'Desativo', value: false}]} />
                 </div>
-                <div className={`${hidden}`}>Endereço</div>
-                <div className={`${hidden} pt-8 flex gap-3`}>
+                <div className={`${hidden} pt-2`}>Endereço</div>
+                <div className={`${hidden} pt-6 flex gap-3`}>
                     <Input
                     initialValue={data  == undefined ? null : data.district}
                     readOnly={lock}
@@ -139,6 +141,162 @@ export default function FuncionariosModalBody(props) {
                     labelPlaceholder="Código Postal"
                     />
                 </div>
+            </Modal.Body>
+            {footer}
+        </div>
+    )
+}
+
+export function FerramentasModalBody(props) {
+    const data = props.data
+    const footer = props.footer
+    const action = props.action
+
+    var lock
+    if (action == 'detalhes') { lock = true }
+
+    var hidden
+    if (action == 'delete') { hidden = 'hidden' }
+
+    return (
+        <div>
+            <Modal.Body>
+                <div className={`${hidden} pt-7 flex gap-3`}>
+                    <Input
+                    initialValue={data == undefined ? null : data.name}
+                    readOnly={lock}
+                    bordered
+                    fullWidth
+                    required
+                    color="primary"
+                    size="lg"
+                    labelPlaceholder="Nome"  
+                    />
+                    <Input
+                    initialValue={data == undefined ? null : data.brand}
+                    readOnly={lock}
+                    bordered
+                    fullWidth
+                    required
+                    color="primary"
+                    size="lg"
+                    labelPlaceholder="Marca"  
+                    />
+                </div>
+                <div className={`${hidden} pt-7 flex gap-3`}>
+                    <Input
+                    initialValue={data == undefined ? null : data.price}
+                    readOnly={lock}
+                    bordered
+                    fullWidth
+                    required
+                    contentLeft="R$"
+                    color="primary"
+                    size="lg"
+                    labelPlaceholder="Preço"  
+                    />
+                    <Input
+                    initialValue={data == undefined ? null : data.avarage_life}
+                    readOnly={lock}
+                    bordered
+                    fullWidth
+                    required
+                    color="primary"
+                    size="lg"
+                    labelPlaceholder="Vida Média"  
+                    />
+                </div>
+                <div className={`${hidden} pt-8 flex gap-3`}>
+                    <Input
+                    initialValue={data == undefined ? null : data.provider}
+                    readOnly={lock}
+                    bordered
+                    fullWidth
+                    required
+                    color="primary"
+                    size="lg"
+                    labelPlaceholder="Fornecedor"  
+                    />
+                </div>
+            </Modal.Body>
+            {footer}
+        </div>
+    )
+}
+
+export function MateriaisModalBody(props) {
+    const data = props.data
+    const footer = props.footer
+    const action = props.action
+
+    var lock
+    if (action == 'detalhes') { lock = true }
+
+    var hidden
+    if (action == 'delete') { hidden = 'hidden' }
+
+    return (
+        <div>
+            <Modal.Body>
+                <div className={`${hidden} pt-7 flex gap-3`}>
+                    <Input
+                    initialValue={data == undefined ? null : data.name}
+                    readOnly={lock}
+                    bordered
+                    fullWidth
+                    required
+                    color="primary"
+                    size="lg"
+                    labelPlaceholder="Nome"  
+                    />
+                </div>
+                <div className={`${hidden} pt-7 flex gap-3`}>
+                    <Input
+                    initialValue={data == undefined ? null : data.price}
+                    readOnly={lock}
+                    bordered
+                    fullWidth
+                    required
+                    contentLeft="R$"
+                    color="primary"
+                    size="lg"
+                    labelPlaceholder="Preço"  
+                    />
+                    <Input
+                    initialValue={data == undefined ? null : data.unit_of_measure_price}
+                    readOnly={lock}
+                    bordered
+                    fullWidth
+                    required
+                    color="primary"
+                    size="lg"
+                    labelPlaceholder="Unidade de Medida"  
+                    />
+                </div>
+            </Modal.Body>
+            {footer}
+        </div>
+    )
+}
+
+export function AcaoFuncionariosModalBody(props) {
+    const data = props.data
+    const footer = props.footer
+    const action = props.action
+
+    var lock
+    if (action == 'detalhes') { lock = true }
+
+    var hidden
+    if (action == 'delete') { hidden = 'hidden' }
+
+    return (
+        <div>
+            <Modal.Body>
+                
+                <ListBoxComponent items={funcionarios} />
+                <ListBoxComponent items={obras} />
+                
             </Modal.Body>
             {footer}
         </div>
