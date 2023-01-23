b5483ef3695e696be7f6d481f810fddef1a3e044
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-23 09:42:40 -0300

    :construction: build: dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
+import { useState } from 'react'
+import Donut from '../components/charts/Donut'
+import { funcionarios } from '../data'
 
 const Dashboard = () => {
+
+  // const [mediaN, setMediaN] = useState(0)
+  // const [mediaS, setMediaS] = useState(0)
+
+  // funcionarios.map(item => {
+  //   setMediaN(mediaN + 1)
+  //   setMediaS(mediaS + item.salary)
+  // })
+
+  // var mediaTotal = mediaS[1] / mediaN[0]
+
     return (
-        <div>Dashboard oi</div>
-  )
+      <div className='flex w-full p-[15px]'>
+        <div className='w-6/12'>
+          <div className='w-[320px] text-sm bg-mainColor rounded-[15px] p-3'>Ola Engenheiro(a), aqui temos algumas métricas que lhe podem ser uteis.</div>
+          <div className='mt-5'>
+            <div>Fases das Obras</div>
+            {/* <Donut/> */}
+          </div>
+        </div>
+        <div>
+          Média da Diaria dos Funcionarios: R$ 140,00
+        </div>
+      </div>
+    )
 }
 
 export default Dashboard
