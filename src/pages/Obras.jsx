9e6616277a0b0fd2df1f2530041e3133dd9af94a
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-23 09:49:35 -0300

    :construction: build: Obras view

---
+++ src/pages/Obras.jsx
@@ -0,0 +1,138 @@
+import { Col, Row, Table, Text, Tooltip} from '@nextui-org/react'
+import axios from 'axios'
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { HeaderFerramentas } from '../components/functions/HeaderCrud'
+import ModalButton from '../components/ModalButton'
+import { ferramentas, obras } from '../data'
+
+const Obras = () => {
+
+  const [datas, setDatas] = useState()
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/tools")
+    .then((response) => setDatas(response.data))
+  })
+  console.log(datas)
+
+  const columns = [
+    { name: 'Nome', key: 'name' },
+    { name: 'Cliente', key: 'cliente' },
+    { name: 'Tipo', key: 'tipo' },
+    {name: 'Actions', key: 'actions'}
+  ]
+
+  const data = obras
+
+  const renderCell = (data, columnKey) => {
+    const cellValue = data[columnKey]
+    switch (columnKey) {
+      case 'name':
+        return (
+          <div className='text-sm'>{data.name}</div>
+        )
+      case 'cliente':
+        return (
+          <div className='text-sm'>{data.client_name}</div>
+        )
+      case 'tipo':
+        return (
+            <div className='border-mainColor border-solid border-2 rounded-full px-6 py-1 text-sm w-fit'>{data.type}</div>
+        )
+      case "actions":
+        return (
+          <Row justify="center" align="center">
+            <Col css={{ d: "flex" }}>
+              <Tooltip content="Detalhes">
+                <ModalButton type='Ferramenta' action='detalhes' data={data} />
+              </Tooltip>
+            </Col>
+            <Col css={{ d: "flex" }}>
+              <Tooltip content="Editar">
+                <ModalButton type='Ferramenta' action='editar' data={data} />
+              </Tooltip>
+            </Col>
+            <Col css={{ d: "flex" }}>
+              <Tooltip content="Delete" color="error">
+                <ModalButton type='Ferramenta' action='delete' data={data} /> 
+              </Tooltip>
+            </Col>
+          </Row>
+        );
+      default:
+        return cellValue
+    }
+  }
+
+  return (
+    <div className='text-white w-full p-[15px]'>
+      <div className='mx-7 my-5 text-lg font-medium'>Ferramentas</div>
+      <HeaderFerramentas data={data} />
+      <div className='relative -top-14 z-10'>
+        <Table
+          aria-label="Example table with custom cells"
+        css={{
+        height: "auto",
+        minWidth: "100%",
+        background: "var(--darkGray)"
+          }}
+          borderWeight="none"
+          headerLined={true}
+          lineWeight="none"
+          color='primary'
+        >
+          <Table.Header columns={columns}>
+            {(column) => (
+              <Table.Column
+                key={column.key}
+                hideHeader={column.key === "actions"}
+                align={column.key === "actions" ? "center" : "start"}
+                
+              >
+                {column.name}
+              </Table.Column>
+            )}
+          </Table.Header>
+          <Table.Body items={data}>
+            {(item) => (
+              <Table.Row>
+                {(columnKey) => (
+                  <Table.Cell css={{
+                    background: "var(--lightGray)",
+                    
+                  }}>{renderCell(item, columnKey)}</Table.Cell>
+                )}
+              </Table.Row>
+            )}
+          </Table.Body>
+        </Table>
+      </div>
+      <style jsx>{`
+        td:first-child,
+        th:first-child {
+          border-radius: 20px 0 0 20px;
+        }
+
+        td:last-child,
+        th:last-child {
+          border-radius: 0 20px 20px 0;
+        }
+
+        table{
+          border-collapse:separate;
+          border-spacing: 0 15px !important; 
+        }
+
+        thead{
+          position: relative;
+          top: 30px;
+          left: 3px
+        }
+
+      `}</style>
+    </div>
+  )
+}
+
+export default Obras
