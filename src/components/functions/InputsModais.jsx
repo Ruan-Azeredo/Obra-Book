a1f336ab51ff0731340baa05c0040ab0ba134da3
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-28 21:11:48 -0300

    :construction: build: getInfos input funcionar.

--- src/components/functions/InputsModais.jsx
+++ src/components/functions/InputsModais.jsx
@@ -1,4 +1,6 @@
-import { Input, Modal, Switch, Textarea } from "@nextui-org/react"
+import { Button, Input, Modal, Switch, Textarea } from "@nextui-org/react"
+import { useState } from "react"
+import { workerPost } from "../../api/workerPost"
 import { ferramentas, funcionarios, materiais, obras } from "../../data"
 import ListBoxComponent from "../ListBoxComponent"
 
@@ -7,25 +9,60 @@ export function FuncionariosModalBody(props) {
     const footer = props.footer
     const action = props.action
 
+    const [firstName, setFirstName] = useState(data == undefined ? null : data.firstName);
+    const [lastName, setLastName] = useState(data == undefined ? null : data.lastName);
+    const [dailyWage, setDailyWage] = useState(data == undefined ? null : data.dailyWage);
+    const [phoneNo, setPhoneNo] = useState(data == undefined ? null : data.phoneNo);
+    const [cpf, setCpf] = useState(data == undefined ? null : data.cpf);
+    const [age, setAge] = useState(data == undefined ? null : data.age);
+    const [type, setType] = useState(data == undefined ? null : data.type);
+    const [status, setStatus] = useState(data == undefined ? 0 : data.status);
+    const [isAssigned, setisAssigned] = useState(data == undefined ? false : data.isAssigned);
+    const [district, setDistrict] = useState(data == undefined ? null : data.district);
+    const [street, setStreet] = useState(data == undefined ? null : data.street);
+    const [number, setNumber] = useState(data == undefined ? null : data.number);
+    const [zip_code, setZipCode] = useState(data == undefined ? null : data.zip_code);
+
     var lock
     if (action == 'detalhes') { lock = true }
 
     var hidden
     if (action == 'delete') { hidden = 'hidden' }
 
+    let obj = {
+        firstName: firstName,
+        lastName: lastName,
+        dailyWage: dailyWage,
+        phoneNo: phoneNo,
+        cpf: cpf,
+        age: age,
+        type: type,
+        status: status,
+        isAssigned: isAssigned,
+        district: district,
+        street: street,
+        number: number,
+        zip_code: zip_code
+    }
+
+    const handleValueListBox = (value) => {
+        setStatus(value.value)
+    }
+
     return (
         <div>
             <Modal.Body>
                 <div className={`${hidden} pt-7 flex gap-3`}>
                     <Input
-                    initialValue={data == undefined ? null : data.name}
+                    initialValue={data == undefined ? null : data.firstName}
                     readOnly={lock}
                     bordered
                     fullWidth
                     required
                     color="primary"
                     size="lg"
-                    labelPlaceholder="Nome"  
+                    labelPlaceholder="Nome"
+                    onChange={(event)=>setFirstName(event.target.value)}    
                     />
                     <Input
                     initialValue={data == undefined ? null : data.lastName}
@@ -35,12 +72,13 @@ export function FuncionariosModalBody(props) {
                     required
                     color="primary"
                     size="lg"
-                    labelPlaceholder="Sobrenome"  
+                    labelPlaceholder="Sobrenome"
+                    onChange={(event)=>setLastName(event.target.value)}     
                     />
                 </div>
                 <div className={`${hidden} pt-7 flex gap-3`}>
                     <Input
-                    initialValue={data == undefined ? null : data.salary}
+                    initialValue={data == undefined ? null : data.dailyWage}
                     readOnly={lock}
                     bordered
                     fullWidth
@@ -49,9 +87,10 @@ export function FuncionariosModalBody(props) {
                     color="primary"
                     size="lg"
                     labelPlaceholder="Salario"  
+                    onChange={(event)=>setDailyWage(event.target.value)} 
                     />
                     <Input
-                    initialValue={data == undefined ? null : data.phone}
+                    initialValue={data == undefined ? null : data.phoneNo}
                     readOnly={lock}
                     bordered
                     fullWidth
@@ -59,6 +98,7 @@ export function FuncionariosModalBody(props) {
                     color="primary"
                     size="lg"
                     labelPlaceholder="Celular"  
+                    onChange={(event)=>setPhoneNo('' + event.target.value)} 
                     />
                 </div>
                 <div className={`${hidden} pt-8 flex gap-3`}>
@@ -71,6 +111,7 @@ export function FuncionariosModalBody(props) {
                     color="primary"
                     size="lg"
                     labelPlaceholder="CPF"  
+                    onChange={(event)=>setCpf(event.target.value)} 
                     />
                     <Input
                     initialValue={data == undefined ? null : data.age}
@@ -81,6 +122,7 @@ export function FuncionariosModalBody(props) {
                     color="primary"
                     size="lg"
                     labelPlaceholder="Idade"  
+                    onChange={(event)=>setAge(event.target.value)} 
                     />
                 </div>
                 <div className={`${hidden} pt-8 flex gap-6`}>
@@ -93,8 +135,9 @@ export function FuncionariosModalBody(props) {
                     color="primary"
                     size="lg"
                     labelPlaceholder="Tipo"
+                    onChange={(event)=>setType(event.target.value)} 
                     />
-                    <ListBoxComponent lock={lock} items={[{name: 'Ativo', value: true}, {name: 'Desativo', value: false}]} />
+                    <ListBoxComponent lock={lock} items={[{ name: 'Ativo', value: 1 }, { name: 'Desativo', value: 0 }]} onValueChange={handleValueListBox} />
                 </div>
                 <div className={`${hidden} pt-2`}>Endereço</div>
                 <div className={`${hidden} pt-6 flex gap-3`}>
@@ -107,6 +150,7 @@ export function FuncionariosModalBody(props) {
                     color="primary"
                     size="lg"
                     labelPlaceholder="Bairro"
+                    onChange={(event)=>setDistrict(event.target.value)} 
                     />
                     <Input
                     initialValue={data  == undefined ? null : data.street}
@@ -117,6 +161,7 @@ export function FuncionariosModalBody(props) {
                     color="primary"
                     size="lg"
                     labelPlaceholder="Rua"
+                    onChange={(event)=>setStreet(event.target.value)} 
                     />
                 </div>
                 <div className={`${hidden} pt-8 flex gap-3`}>
@@ -129,6 +174,7 @@ export function FuncionariosModalBody(props) {
                     color="primary"
                     size="lg"
                     labelPlaceholder="Número"
+                    onChange={(event)=>setNumber(event.target.value)} 
                     />
                     <Input
                     initialValue={data  == undefined ? null : data.zip_code}
@@ -139,9 +185,14 @@ export function FuncionariosModalBody(props) {
                     color="primary"
                     size="lg"
                     labelPlaceholder="Código Postal"
+                    onChange={(event)=>setZipCode(event.target.value)} 
                     />
                 </div>
             </Modal.Body>
+            <Button onClick={() => {
+                console.log(obj)
+                workerPost(obj)
+            }}>Console.log</Button>
             {footer}
         </div>
     )
