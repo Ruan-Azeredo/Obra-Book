9192097472dddf7623e67a8440dbf1511875f366
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-29 22:57:22 -0300

    :construction: build: redirect login

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { useContext } from 'react';
 import { useEffect, useState } from 'react';
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, useLocation, redirect, Navigate, useNavigate } from 'react-router-dom';
 import './App.css';
 import Sidebar from './components/Sidebar';
+import ObjecttContext from './contexts/ObjecttContext';
 import Clientes from './pages/Clientes';
 import Dashboard from './pages/Dashboard';
 import Feramentas from './pages/Feramentas';
@@ -13,32 +15,48 @@ import Planejamento from './pages/Planejamento';
 import Relatorio from './pages/Relatorio';
 
 function App() {
-  // const [hideSidebar, setHideSidebar] = useState(false);
-  // const location = useLocation();
+  const { EngineerId } = useContext(ObjecttContext)
 
-  // useEffect(() => {
-  //   if (location.pathname == '/login') {
-  //     setHideSidebar('hidden');
-  //   }
-  // }, [location]);
+  const [hideSidebar, setHideSidebar] = useState('hidden');
+  const location = document.location.pathname
+  console.log('location: ', location)
+
+  const [rout, setRout] = useState()
+  useEffect(() => {
+    if (location != '/login' && EngineerId == true) {
+      setHideSidebar('');
+      setRout(
+        null
+      )
+    } else if(location != '/login'){
+      setRout(
+        <Navigate replace to='/login' />
+      )
+    } else {
+      setRout(null)
+    }
+  }, [location, rout])
 
   return (
     <div className='flex'>
       <BrowserRouter>
-        <div>
+        <div className={`${hideSidebar}`}>
           <Sidebar/>
         </div>
         <div className='w-full'>
+        <Routes>
+          <Route element={<Dashboard />} path='/dashboard' />
+          <Route element={<Funcionarios />} path='/funcionarios' />
+          <Route element={<Feramentas />} path='/ferramentas' />
+          <Route element={<Materiais />} path='/materiais' />
+          <Route element={<Clientes />} path='/clientes' />
+          <Route element={<Obras />} path='/obras' />
+          <Route element={<Planejamento />} path='/planejamento' />
+          <Route element={<Relatorio />} path='/relatorio' />
+        </Routes>
+            {rout}
           <Routes>
             <Route element={<Login />} path='/login' />
-            <Route element={<Dashboard />} path='/dashboard' />
-            <Route element={<Funcionarios />} path='/funcionarios' />
-            <Route element={<Feramentas />} path='/ferramentas' />
-            <Route element={<Materiais />} path='/materiais' />
-            <Route element={<Clientes />} path='/clientes' />
-            <Route element={<Obras/>} path='/obras' />
-            <Route element={<Planejamento />} path='/planejamento' />
-            <Route element={<Relatorio/>} path='/relatorio'/>
           </Routes>
         </div>
       </BrowserRouter>
