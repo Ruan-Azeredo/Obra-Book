81d08b168a19895722239e26696f07ebd5429168
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-29 22:55:19 -0300

    :construction: build: adiciona EngineerId

--- src/contexts/ObjecttContext.jsx
+++ src/contexts/ObjecttContext.jsx
@@ -4,13 +4,15 @@ const ObjecttContext = createContext()
 
 export function ObjecttProvider(props) {
     const [ObjecttGlobal, setObjecttGlobal] = useState()
+    const [EngineerId, setEngineerId] = useState(true) //para manter logado em produção
 
-    console.log(ObjecttGlobal)
+    console.log(EngineerId)
 
 
     return (
         <ObjecttContext.Provider value={{
-            ObjecttGlobal, setObjecttGlobal
+            ObjecttGlobal, setObjecttGlobal,
+            EngineerId, setEngineerId
         }}>
             {props.children}
         </ObjecttContext.Provider>
