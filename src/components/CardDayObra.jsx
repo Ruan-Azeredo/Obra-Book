0128b0e40201837ff87291baf7e9496df83a6989
Ruan-Azeredo <ruanazeredo@gmail.com>
2023-01-31 23:16:58 -0300

    :goal_net: fix: reorganiza variaveis

--- src/components/CardDayObra.jsx
+++ src/components/CardDayObra.jsx
@@ -1,35 +1,41 @@
-import { Card } from '@nextui-org/react'
+import { Card, Tooltip } from '@nextui-org/react'
 import React from 'react'
-import { acaoFerramentas, acaoFuncionarios, acaoMateriais } from '../data'
 import { buildingLibrary, buildingOffice, homeModern } from './icons'
 
 const CardDayObra = (props) => {
     const data = props.data
     const dia = props.dia
     const typeBuilding = data.type
+    const acaoFuncionarios = props.acaoFuncionarios
+    const acaoFerramentas = props.acaoFerramentas
+    const acaoMateriais = props.acaoMateriais
+    const funcionarios = props.funcionarios
+    const ferramentas = props.ferramentas
+    const materiais = props.materiais
 
     var funcionariosPerDay = []
     acaoFuncionarios.map(item => {
-        if (item.date == dia.date && item.construction_id == data.id) {
+        if (item.data == dia && item.constructionID == data.id) {
             funcionariosPerDay.push(item)
         }
     })
 
     var ferramentasPerDay = []
     acaoFerramentas.map(item => {
-        if (item.date == dia.date && item.construction_id == data.id) {
+        if (item.data == dia && item.constructionID == data.id) {
             ferramentasPerDay.push(item)
         }
     })
 
     var materiaisPerDay = []
     acaoMateriais.map(item => {
-        if (item.date == dia.date && item.construction_id == data.id) {
+        if (item.data == dia && item.constructionID == data.id) {
             materiaisPerDay.push(item)
         }
     })
 
     var hidden
+    var na
 
     if (funcionariosPerDay[0] == undefined && ferramentasPerDay[0] == undefined && materiaisPerDay[0] == undefined) {
         hidden = 'hidden'
@@ -51,30 +57,53 @@ const CardDayObra = (props) => {
                     <div className='rounded-lg bg-background p-1 m-[3px]'>{icon}</div>
                     <div className='py-[6px] px-2'>{data.name}</div>
                 </div>
-                <div className='text-gray-400 text-sm mt-[2px] px-3 py-[6px]'>{dia.date}</div>
+                <div className='text-gray-400 text-sm mt-[2px] px-3 py-[6px]'>{dia}</div>
             </div>
             <Card css={{background: "var(--darkGray)", border: "none", borderRadius: "8px"}}>
                 <Card.Body>
                     <div className='flex justify-between'>
                         <div>
                             {funcionariosPerDay.map(item => (
-                                <div className='bg-mainColor rounded-full px-3 mb-4'>{item.employee_name}</div>
+                                <div>
+                                    {
+                                        funcionarios.map(worker => {
+                                            if (item.workerID == worker.id) {
+                                            na = worker.firstName
+                                            }
+                                        })
+                                    }
+                                    <Tooltip content={item.description}>
+                                        <div className='bg-mainColor rounded-full px-3 mb-4'>{na}</div>
+                                    </Tooltip>
+                                </div>
                             ))}
                         </div>
                         <div>
                         {ferramentasPerDay.map(item => (
                             <div className='bg-background rounded-full px-3 flex gap-2'>
-                                <span className='text-mainColor font-bold'>{item.amount} </span>
-                                <span>{item.tool_name}</span>
+                                {
+                                    ferramentas.map(tool => {
+                                        if (item.toolID == tool.id) {
+                                        na = tool.name
+                                        }
+                                    })
+                                }
+                                <span>{na}</span>
                             </div>
                         ))}
                         </div>
                         <div>
                         {materiaisPerDay.map(item => (
                             <div className='bg-lightGray rounded-lg px-3 py-1'>
-                                <span className='text-mainColor font-semibold'>{item.amount} </span>
-                                <span>{item.unit} </span>
-                                <div>{item.material_name}</div>
+                                {
+                                    materiais.map(supply => {
+                                        if (item.supplyID == supply.id) {
+                                        na = supply.name
+                                        }
+                                    })
+                                }
+                                <span className='text-mainColor font-semibold justify-center flex'>{item.quantity} </span>
+                                <div>{na}</div>
                             </div>
                         ))}
                         </div>
